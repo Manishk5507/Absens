1e7b0fd35efee5a2b448e46a3a490175fa4b69a4
VaibhavVermaa16 <vaibhavvermaa16@gmail.com>
2024-10-19 19:50:48 +0530

    Revert "Revert "Added chatbot trained on our project""
    This reverts commit 1170dc22b6483788060aa965f2628546228377c8.

---
+++ Frontend/src/ChatBot/Chatbot.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import ChatbotIcon from "../assets/chatbot.png";
+
+const Chatbot = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        onClick={toggleChatbot}
+        className="fixed bottom-4 right-4 p-3 bg-white rounded-full shadow-lg text-white hover:bg-slate-400 focus:outline-none"
+      >
+        <img src={ChatbotIcon} alt="Chatbot" className="w-8 h-8" />
+      </button>
+
+      {/* Chatbot Iframe */}
+      {isOpen && (
+        <div className="fixed bottom-16 right-4 w-[90vw] h-[70vh] md:w-[30vw] md:h-[70vh] bg-white shadow-lg rounded-lg overflow-hidden">
+          <iframe
+            width="100%"
+            height="100%"
+            allow="microphone;"
+            src="https://console.dialogflow.com/api-client/demo/embedded/ea0b68db-8adf-40b5-a25f-03595aa270ad"
+            title="Chatbot"
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Chatbot;
